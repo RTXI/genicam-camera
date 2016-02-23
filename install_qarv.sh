418e284584129dcf7af27e422bafe926cb1b1518
anselg <aesthapus@gmail.com>
2016-02-23 14:26:41 -0500

    using my fork of qarv

--- install_qarv.sh
+++ install_qarv.sh
@@ -5,7 +5,7 @@ sudo apt-get install \
 	cmake libswscale-dev libavutil-dev libavcodec-dev libopencv-dev
 
 # clone reop and build
-git clone https://github.com/AD-Vega/qarv
+git clone https://github.com/anselg/qarv
 cd qarv
 mkdir build
 cd build
