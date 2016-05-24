3cf41196d631aaaa7b9d41e2fc8e679dd4316231
Ansel George <ang2049@med.cornell.edu>
2016-05-24 19:11:52 -0400

    need to use netctl to set up network connection for ethernet cameras

--- scripts/set_camera_ipaddr.sh
+++ scripts/set_camera_ipaddr.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
 
-sudo apt-get install net-tools
+################################################################################
+# NEEDS ATTENTION. Using nettools to set the ip address will cause conflicts 
+# with networkmanager and cause the connection to the ethernet camera to drop. 
+# Instead, define another connection via networkmanager. 
+#
+# You'll still need to manually set the mtu to 9000 via ifconfig for now...
+################################################################################
 
-# sudo ifconfig eth1 169.254.1.1 
-# sudo ifconfig eth1 mtu 9000
+sudo ifconfig
+
+echo "--> Which device corresponds to your camera?"
+read answer
+
+if [[ "$answer" == eth[0-9] ]]; then 
+	sudo ifconfig $answer mtu 9000
+else 
+	echo "ERROR: $answer"
+fi
